58f4a578f50bc2cea02515ea790aaa93d36d0375
Huy <giahuychau07@gmail.com>
2026-07-13 23:09:01 +0700

    Simple Hello World request

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+var server = new TcpListener(IPAddress.Any, 8000);
+server.Start();
+Console.WriteLine("Connection started");
+
+while (true) {
+    var client = await server.AcceptTcpClientAsync();
+    var stream = client.GetStream();
+
+    var buffer = new byte[1024];
+    await stream.ReadAsync(buffer);
+
+    var response = "HTTP/1.1 200 OK\r\nContent-Length: 11\r\n\r\nHello World";
+    var bytes = Encoding.UTF8.GetBytes(response);
+    await stream.WriteAsync(bytes);
+    client.Close();
+}
